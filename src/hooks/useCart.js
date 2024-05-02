9beeaaf4cbfa50fb59d7e39d6ac3a2a0f36f1bbc
user <user@users-MacBook-Pro.local>
2024-05-02 12:17:07 -0500

    Subiendo proyecto carrito de compras

---
+++ src/hooks/useCart.js
@@ -0,0 +1,68 @@
+
+
+export const useCart = () => {
+
+    const initialCart = () => {
+        const localStorageCart = localStorage.getItem('cart')
+        return localStorageCart ? JSON.parse(localStorageCart) : []
+    }
+
+    const [data, setData] = useState(db)
+    const [cart, setCart] = useState(initialCart)
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
+    function addToCart(item) {
+
+        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+        if (itemExists >= 0) {
+            if (cart[itemExists].quantity >= 5) return
+            const updatedCart = [...cart]
+            updatedCart[itemExists].quantity++
+            setCart(updatedCart)
+        } else {
+            item.quantity = 1
+            setCart(prevCart => [...prevCart, item])
+        }
+    }
+    function removeFromCart(id) {
+        setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+    }
+
+    function increaseQuantity(id) {
+        const updatedCart = cart.map(item => {
+            if (item.id === id && item.quantity < 5) {
+                return {
+                    ...item,
+                    quantity: item.quantity + 1
+                }
+            }
+            return item
+        })
+        setCart(updatedCart)
+    }
+
+    function decreaseQuantity(id) {
+        const updatedCart = cart.map(item => {
+            if (item.id === id && item.quantity > 1) {
+                return {
+                    ...item,
+                    quantity: item.quantity - 1
+                }
+            }
+            return item
+        })
+        setCart(updatedCart)
+    }
+
+    function clearCart() {
+        setCart([])
+    }
+
+    return{
+
+    }
+    
+}
